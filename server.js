583ef0dd3999c80acec8e525a2554c044a7d7421
Agustin Donalisio <donalisioagustin@gmail.com>
2023-04-23 17:21:14 -0300

    Adding server.js
    I'm new in node.js but i think i got it now, so create the server.js files that load html,css,js files on demand to work locally

---
+++ server.js
@@ -0,0 +1,100 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(`Request for ${req.url} received.`);
+
+    //loading index on demand
+    if (req.url === '/') {
+        const indexFilePath = path.join(__dirname, 'index.html');
+        fs.readFile(indexFilePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Error loading ${indexFilePath}: ${err}`);
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else if (req.url === '/index.html') {
+        const cssPath = path.join(__dirname, 'index.html');
+        fs.readFile(cssPath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Error loading ${cssPath}: ${err}`);
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+
+        //loading style.css on demand
+    } else if (req.url === '/style.css') {
+        const cssPath = path.join(__dirname, 'style.css');
+        fs.readFile(cssPath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Error loading ${cssPath}: ${err}`);
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        });
+
+        // loading intakes.html on demand
+    } else if (req.url === '/intakes.html') {
+        const cssPath = path.join(__dirname, 'intakes.html');
+        fs.readFile(cssPath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Error loading ${cssPath}: ${err}`);
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+
+        //loading exits.html on demand
+    } else if (req.url === '/exits.html') {
+        const cssPath = path.join(__dirname, 'exits.html');
+        fs.readFile(cssPath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Error loading ${cssPath}: ${err}`);
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+
+        //loading app.js on demand
+    } else if (req.url === '/style.css') {
+        const cssPath = path.join(__dirname, 'style.css');
+        fs.readFile(cssPath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Error loading ${cssPath}: ${err}`);
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end(`Resource not found: ${req.url}`);
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
