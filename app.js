befe4610119b3d4f83a248bf0c7c79b773ab916f
Agustin Donalisio <donalisioagustin@gmail.com>
2023-04-25 01:38:39 -0300

    Adding validations to form

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//this one is used for an input, to start the initial value at current year
 let cont = 0
 function maxYear(e) {
     let currYear = new Date().getFullYear()
@@ -20,12 +21,14 @@ function maxYear(e) {
     e.value = x()
 }
 
+//this one is to fix the max date as current date
 function maxFullDate(e) {
     if (e.value > getFullDate()) {
         e.value = getFullDate()
     }
 }
 
+//this function returns the current full date in a string year-month-day
 function getFullDate() {
     let currentDate = new Date()
     const year = currentDate.getFullYear();
@@ -34,4 +37,41 @@ function getFullDate() {
     const dateString = `${year}-${month}-${day}`;
     
     return dateString
+}
+
+//this function receives a dom element start the value on 1
+let cont1 = 0
+function initialQuantity(e) {
+    const INITIAL_VALUE = 1
+    if (cont1 === 0) {
+        e.value = INITIAL_VALUE
+        cont1++
+    }
+}
+
+//this function receives a dom element and start the value on the current year
+let cont2 = 0
+function intialYear(e) {
+    const currYear = new Date().getFullYear()
+    if (cont2 === 0) {
+        e.value = currYear
+        cont2++
+    }
+}
+
+//this handle the label and input to add a wine type if not in select list
+function handleSelect(e) {
+    let elements = document.getElementsByName('newType')
+    if (e.value === 'addType') {
+        elements.forEach(i => {
+            i.style.display = "block"
+            i.required = true
+        });
+    }
+    if(e.value !== 'addType'){
+        elements.forEach(i => {
+            i.style.display = "none"
+            i.required = false
+        });
+    }
 }
