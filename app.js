0a5fc3921a2534aa905601484c8897675ce41eda
Agustin Donalisio <donalisioagustin@gmail.com>
2023-04-25 02:33:08 -0300

    More Js validations
    Making bodega and wine type inputs as select, so now it display a list for current bodegas/wine tipes, and if is not in the list, the user select the option 'add option' and shows the label and input to add that option, looking forward to do the back end part

--- app.js
+++ app.js
@@ -1,77 +1,129 @@
+// this is for the title of the document; it gets the current date and time and updates it every second
+setInterval(() => {
+    document.title = getCurrentDateTime();
+}, 1000);
+
 //this one is used for an input, to start the initial value at current year
-let cont = 0
+let cont = 0;
 function maxYear(e) {
-    let currYear = new Date().getFullYear()
-    e.max = currYear
+    let currYear = new Date().getFullYear();
+    e.max = currYear;
 
     function x() {
-
         if (cont === 0) {
-            cont++
-            return currYear
+            cont++;
+            return currYear;
         } else if (e.value < 1930) {
-            return 1930
+            return 1930;
         } else if (e.value <= currYear) {
-            return e.value
+            return e.value;
         } else {
-            return currYear
+            return currYear;
         }
     }
 
-    e.value = x()
+    e.value = x();
 }
 
 //this one is to fix the max date as current date
 function maxFullDate(e) {
     if (e.value > getFullDate()) {
-        e.value = getFullDate()
+        e.value = getFullDate();
     }
 }
 
 //this function returns the current full date in a string year-month-day
 function getFullDate() {
-    let currentDate = new Date()
+    let currentDate = new Date();
     const year = currentDate.getFullYear();
-    const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
-    const day = ('0' + currentDate.getDate()).slice(-2);
+    const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+    const day = ("0" + currentDate.getDate()).slice(-2);
     const dateString = `${year}-${month}-${day}`;
-    
-    return dateString
+
+    return dateString;
 }
 
 //this function receives a dom element start the value on 1
-let cont1 = 0
+let cont1 = 0;
 function initialQuantity(e) {
-    const INITIAL_VALUE = 1
+    const INITIAL_VALUE = 1;
     if (cont1 === 0) {
-        e.value = INITIAL_VALUE
-        cont1++
+        e.value = INITIAL_VALUE;
+        cont1++;
     }
 }
 
 //this function receives a dom element and start the value on the current year
-let cont2 = 0
+let cont2 = 0;
 function intialYear(e) {
-    const currYear = new Date().getFullYear()
+    const currYear = new Date().getFullYear();
     if (cont2 === 0) {
-        e.value = currYear
-        cont2++
+        e.value = currYear;
+        cont2++;
     }
 }
 
-//this handle the label and input to add a wine type if not in select list
-function handleSelect(e) {
-    let elements = document.getElementsByName('newType')
-    if (e.value === 'addType') {
-        elements.forEach(i => {
-            i.style.display = "block"
-            i.required = true
+// this code snippet handles the label and input for adding a new WINE TYPE if it is not already included in the select list
+function handleSelectWineType(e) {
+    let elements = document.getElementsByName("newType");
+    if (e.value === "addType") {
+        elements.forEach((i) => {
+            i.style.display = "block";
+            i.required = true;
         });
     }
-    if(e.value !== 'addType'){
-        elements.forEach(i => {
-            i.style.display = "none"
-            i.required = false
+    if (e.value !== "addType") {
+        elements.forEach((i) => {
+            i.style.display = "none";
+            i.required = false;
         });
     }
+}
+
+// this code snippet handles the label and input for adding a new BODEGA if it is not already included in the select list
+function handleSelectBodega(e) {
+    let elements = document.getElementsByName("newBodega");
+    if (e.value === "addBodega") {
+        elements.forEach((i) => {
+            i.style.display = "block";
+            i.required = true;
+        });
+    }
+    if (e.value !== "addBodega") {
+        elements.forEach((i) => {
+            i.style.display = "none";
+            i.required = false;
+        });
+    }
+}
+
+//this function gives the actual date-time and updates every second
+function getCurrentDateTime() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    var dateString = day + "/" + month + "/" + year;
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    return dateString + " - " + timeString;
 }
