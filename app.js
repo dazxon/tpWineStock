a597c92fe830a718e977f0fd6f896f328ebd4a29
dazxon <donalisioagustin@gmail.com>
2023-05-03 16:45:26 -0300

    Toy haciendo *** con git

--- app.js
+++ app.js
@@ -1,203 +1,202 @@
 // this is for the title of the document; it gets the current date and time and updates it every second
 setInterval(() => {
-    document.title = getCurrentDateTime();
+  document.title = getCurrentDateTime();
 }, 1000);
 
 //this one is used for an input, to start the initial value at current year
 let cont = 0;
 function maxYear(e) {
-    let currYear = new Date().getFullYear();
-    e.max = currYear;
+  let currYear = new Date().getFullYear();
+  e.max = currYear;
 
-    function x() {
-        if (cont === 0) {
-            cont++;
-            return currYear;
-        } else if (e.value < 1930) {
-            return 1930;
-        } else if (e.value <= currYear) {
-            return e.value;
-        } else {
-            return currYear;
-        }
+  function x() {
+    if (cont === 0) {
+      cont++;
+      return currYear;
+    } else if (e.value < 1930) {
+      return 1930;
+    } else if (e.value <= currYear) {
+      return e.value;
+    } else {
+      return currYear;
     }
+  }
 
-    e.value = x();
+  e.value = x();
 }
 
 //this one is to fix the max date as current date
 function maxFullDate(e) {
-    if (e.value > getFullDate() || !e.value) {
-        e.value = getFullDate();
-    }
+  if (e.value > getFullDate() || !e.value) {
+    e.value = getFullDate();
+  }
 }
 
 //this function returns the current full date in a string year-month-day
 function getFullDate() {
-    let currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-    const day = ("0" + currentDate.getDate()).slice(-2);
-    const dateString = `${year}-${month}-${day}`;
+  let currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+  const day = ("0" + currentDate.getDate()).slice(-2);
+  const dateString = `${year}-${month}-${day}`;
 
-    return dateString;
+  return dateString;
 }
 
 //this function receives a dom element start the value on 1
 let cont1 = 0;
 function initialQuantity(e) {
-    const INITIAL_VALUE = 1;
-    if (cont1 === 0 || !e.value) {
-        e.value = INITIAL_VALUE;
-        cont1++;
-    }
+  const INITIAL_VALUE = 1;
+  if (cont1 === 0 || !e.value) {
+    e.value = INITIAL_VALUE;
+    cont1++;
+  }
 }
 
 //this function receives a dom element and start the value on the current year
 let cont2 = 0;
 function intialYear(e) {
-    const currYear = new Date().getFullYear();
-    if (cont2 === 0 || !e.value) {
-        e.value = currYear;
-        cont2++;
-    }
+  const currYear = new Date().getFullYear();
+  if (cont2 === 0 || !e.value) {
+    e.value = currYear;
+    cont2++;
+  }
 }
 
 // this code snippet handles the label and input for adding a new WINE TYPE if it is not already included in the select list
 function handleSelectWineType(e) {
-    let elements = document.getElementsByName("newType");
-    if (e.value === "addType") {
-        elements.forEach((i) => {
-            i.style.display = "block";
-            i.required = true;
-        });
-    }
-    if (e.value !== "addType") {
-        elements.forEach((i) => {
-            i.style.display = "none";
-            i.required = false;
-        });
-    }
+  let elements = document.getElementsByName("newType");
+  if (e.value === "addType") {
+    elements.forEach((i) => {
+      i.style.display = "block";
+      i.required = true;
+    });
+  }
+  if (e.value !== "addType") {
+    elements.forEach((i) => {
+      i.style.display = "none";
+      i.required = false;
+    });
+  }
 }
 
 // this code snippet handles the label and input for adding a new BODEGA if it is not already included in the select list
 function handleSelectBodega(e) {
-    let elements = document.getElementsByName("newBodega");
-    if (e.value === "addBodega") {
-        elements.forEach((i) => {
-            i.style.display = "block";
-            i.required = true;
-        });
-    }
-    if (e.value !== "addBodega") {
-        elements.forEach((i) => {
-            i.style.display = "none";
-            i.required = false;
-        });
-    }
+  let elements = document.getElementsByName("newBodega");
+  if (e.value === "addBodega") {
+    elements.forEach((i) => {
+      i.style.display = "block";
+      i.required = true;
+    });
+  }
+  if (e.value !== "addBodega") {
+    elements.forEach((i) => {
+      i.style.display = "none";
+      i.required = false;
+    });
+  }
 }
 
 //this function gives the actual date-time and updates every second
 function getCurrentDateTime() {
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
 
-    if (day < 10) {
-        day = "0" + day;
-    }
-    if (month < 10) {
-        month = "0" + month;
-    }
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
+  if (day < 10) {
+    day = "0" + day;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
 
-    var dateString = day + "/" + month + "/" + year;
-    var timeString = hours + ":" + minutes + ":" + seconds;
-    return dateString + " - " + timeString;
+  var dateString = day + "/" + month + "/" + year;
+  var timeString = hours + ":" + minutes + ":" + seconds;
+  return dateString + " - " + timeString;
 }
 
 // this is for making the checkmark appear when input value is ok
 let check01 = document.getElementById("checkOne");
 function validateCheckmark(elem) {
-    setTimeout(() => {
-        if (elem.value) {
-            elem.classList.add("inputOk");
-        } else {
-            elem.classList.remove("inputOk");
-        }
-    }, 100);
+  setTimeout(() => {
+    if (elem.value) {
+      elem.classList.add("inputOk");
+    } else {
+      elem.classList.remove("inputOk");
+    }
+  }, 100);
 }
 
 //this function is to populate the wine types select
 function populateWineType() {
-    const selectElement = document.getElementById("listWineType");
-    const addedValues = new Set(); // Keep track of added values
-  
-    fetch(
-      "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        data.forEach((obj) => {
-          const value = obj.wine_type;
-          if (!addedValues.has(value)) { // Check if value has already been added
-            const optionElement = document.createElement("option");
-            optionElement.value = value;
-            optionElement.textContent = value.toUpperCase();
-            selectElement.appendChild(optionElement);
-            addedValues.add(value); // Add value to set of added values
-          }
-        });
-      })
-      .catch((error) => console.error(error));
-  }
-  
+  const selectElement = document.getElementById("listWineType");
+  const addedValues = new Set(); // Keep track of added values
+
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        const value = obj.wine_type;
+        if (!addedValues.has(value)) {
+          // Check if value has already been added
+          const optionElement = document.createElement("option");
+          optionElement.value = value;
+          optionElement.textContent = value.toUpperCase();
+          selectElement.appendChild(optionElement);
+          addedValues.add(value); // Add value to set of added values
+        }
+      });
+    })
+    .catch((error) => console.error(error));
+}
 
 function populateBodega() {
-    const selectElement = document.getElementById("intakeBodegaName");
+  const selectElement = document.getElementById("intakeBodegaName");
 
-    fetch(
-        "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((obj) => {
-                const optionElement = document.createElement("option");
-                optionElement.value = obj.wine_bodega;
-                optionElement.textContent = obj.wine_bodega.toUpperCase();
-                selectElement.appendChild(optionElement);
-            });
-        })
-        .catch((error) => console.error(error));
-
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = obj.wine_bodega;
+        optionElement.textContent = obj.wine_bodega.toUpperCase();
+        selectElement.appendChild(optionElement);
+      });
+    })
+    .catch((error) => console.error(error));
 }
 
 function populateWineNames() {
-    const selectElement = document.getElementById("wine-name-options");
+  const selectElement = document.getElementById("wine-name-options");
 
-    fetch(
-        "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((obj) => {
-                const optionElement = document.createElement("option");
-                optionElement.value = obj.wine_name.toUpperCase();
-                optionElement.textContent = obj.wine_type;
-                selectElement.appendChild(optionElement);
-            });
-        })
-        .catch((error) => console.error(error));
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = obj.wine_name.toUpperCase();
+        optionElement.textContent = obj.wine_type;
+        selectElement.appendChild(optionElement);
+      });
+    })
+    .catch((error) => console.error(error));
 }
--- server.js
+++ server.js
@@ -6,109 +6,109 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(`Request for ${req.url} received.`);
+  console.log(`Request for ${req.url} received.`);
 
-    //loading index on demand
-    if (req.url === "/") {
-        const indexFilePath = path.join(__dirname, "index.html");
-        fs.readFile(indexFilePath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${indexFilePath}: ${err}`);
-                return;
-            }
+  //loading index on demand
+  if (req.url === "/") {
+    const indexFilePath = path.join(__dirname, "index.html");
+    fs.readFile(indexFilePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${indexFilePath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        });
-    } else if (req.url === "/index.html") {
-        const cssPath = path.join(__dirname, "index.html");
-        fs.readFile(cssPath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${cssPath}: ${err}`);
-                return;
-            }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.url === "/index.html") {
+    const cssPath = path.join(__dirname, "index.html");
+    fs.readFile(cssPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${cssPath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        });
-    } else if (req.url === "/index.html?") {
-        const cssPath = path.join(__dirname, "index.html");
-        fs.readFile(cssPath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${cssPath}: ${err}`);
-                return;
-            }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.url === "/index.html?") {
+    const cssPath = path.join(__dirname, "index.html");
+    fs.readFile(cssPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${cssPath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
 
-        //loading style.css on demand
-    } else if (req.url === "/style.css") {
-        const cssPath = path.join(__dirname, "style.css");
-        fs.readFile(cssPath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${cssPath}: ${err}`);
-                return;
-            }
+    //loading style.css on demand
+  } else if (req.url === "/style.css") {
+    const cssPath = path.join(__dirname, "style.css");
+    fs.readFile(cssPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${cssPath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/css" });
-            res.end(data);
-        });
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
 
-        // loading intakes.html on demand
-    } else if (req.url === "/intakes.html") {
-        const cssPath = path.join(__dirname, "intakes.html");
-        fs.readFile(cssPath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${cssPath}: ${err}`);
-                return;
-            }
+    // loading intakes.html on demand
+  } else if (req.url === "/intakes.html") {
+    const cssPath = path.join(__dirname, "intakes.html");
+    fs.readFile(cssPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${cssPath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
 
-        //loading exits.html on demand
-    } else if (req.url === "/exits.html") {
-        const cssPath = path.join(__dirname, "exits.html");
-        fs.readFile(cssPath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${cssPath}: ${err}`);
-                return;
-            }
+    //loading exits.html on demand
+  } else if (req.url === "/exits.html") {
+    const cssPath = path.join(__dirname, "exits.html");
+    fs.readFile(cssPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${cssPath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
 
-        //loading app.js on demand
-    } else if (req.url === "/app.js") {
-        const cssPath = path.join(__dirname, "app.js");
-        fs.readFile(cssPath, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end(`Error loading ${cssPath}: ${err}`);
-                return;
-            }
+    //loading app.js on demand
+  } else if (req.url === "/app.js") {
+    const cssPath = path.join(__dirname, "app.js");
+    fs.readFile(cssPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Error loading ${cssPath}: ${err}`);
+        return;
+      }
 
-            res.writeHead(200, { "Content-Type": "text/javascript" });
-            res.end(data);
-        });
-    } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end(`Resource not found: ${req.url}`);
-    }
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end(`Resource not found: ${req.url}`);
+  }
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 //connection to database
@@ -116,17 +116,17 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "master",
-    password: "Donita0708",
-    database: "tpwinestock",
+  host: "localhost",
+  user: "master",
+  password: "Donita0708",
+  database: "tpwinestock",
 });
 
 // Test the connection
 connection.connect((error) => {
-    if (error) {
-        console.error("Error connecting to database: ", error);
-    } else {
-        console.log("Connected to database!");
-    }
+  if (error) {
+    console.error("Error connecting to database: ", error);
+  } else {
+    console.log("Connected to database!");
+  }
 });
