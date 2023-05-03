5df5cc7173a9ebe67fa55b7569254305a6f1b7ae
Agustin Donalisio <donalisioagustin@gmail.com>
2023-05-03 18:58:47 -0300

    merge

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ function maxYear(e) {
 
 //this one is to fix the max date as current date
 function maxFullDate(e) {
-    if (e.value > getFullDate()) {
-        e.value = getFullDate();
-    }
+  if (e.value > getFullDate()) {
+    e.value = getFullDate();
+  }
 }
 
 //this function returns the current full date in a string year-month-day
@@ -46,37 +46,21 @@ function getFullDate() {
 //this function receives a dom element start the value on 1
 let cont1 = 0;
 function initialQuantity(e) {
-<<<<<<< HEAD
   const INITIAL_VALUE = 1;
   if (cont1 === 0 || !e.value) {
     e.value = INITIAL_VALUE;
     cont1++;
   }
-=======
-    const INITIAL_VALUE = 1;
-    if (cont1 === 0 || !e.value) {
-        e.value = INITIAL_VALUE;
-        cont1++;
-    }
->>>>>>> 6e4f645ed6336acd6ce721682497cc436a06ed5b
 }
 
 //this function receives a dom element and start the value on the current year
 let cont2 = 0;
 function intialYear(e) {
-<<<<<<< HEAD
   const currYear = new Date().getFullYear();
   if (cont2 === 0 || !e.value) {
     e.value = currYear;
     cont2++;
   }
-=======
-    const currYear = new Date().getFullYear();
-    if (cont2 === 0 || !e.value) {
-        e.value = currYear;
-        cont2++;
-    }
->>>>>>> 6e4f645ed6336acd6ce721682497cc436a06ed5b
 }
 
 // this code snippet handles the label and input for adding a new WINE TYPE if it is not already included in the select list
@@ -145,3 +129,75 @@ function getCurrentDateTime() {
 }
 
 // here there are the custom invalid message of the forms
+// this is for making the checkmark appear when input value is ok
+let check01 = document.getElementById("checkOne");
+function validateCheckmark(elem) {
+  setTimeout(() => {
+    if (elem.value) {
+      elem.classList.add("inputOk");
+    } else {
+      elem.classList.remove("inputOk");
+    }
+  }, 100);
+}
+
+//this function is to populate the wine types select
+function populateWineType() {
+  const selectElement = document.getElementById("listWineType");
+  const addedValues = new Set(); // Keep track of added values
+
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        const value = obj.wine_type;
+        if (!addedValues.has(value)) {
+          // Check if value has already been added
+          const optionElement = document.createElement("option");
+          optionElement.value = value;
+          optionElement.textContent = value.toUpperCase();
+          selectElement.appendChild(optionElement);
+          addedValues.add(value); // Add value to set of added values
+        }
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+function populateBodega() {
+  const selectElement = document.getElementById("intakeBodegaName");
+
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = obj.wine_bodega;
+        optionElement.textContent = obj.wine_bodega.toUpperCase();
+        selectElement.appendChild(optionElement);
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+function populateWineNames() {
+  const selectElement = document.getElementById("wine-name-options");
+
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/application-0-bqccj/endpoint/wines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = obj.wine_name.toUpperCase();
+        optionElement.textContent = obj.wine_type;
+        selectElement.appendChild(optionElement);
+      });
+    })
+    .catch((error) => console.error(error));
+}
